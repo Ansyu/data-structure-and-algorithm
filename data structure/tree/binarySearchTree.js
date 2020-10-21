5c228ebe4a7c8faaf966509023a79bb10906d4a1
ansyu <haixu.an@outlook.com>
2020-10-20 23:35:11 -0700

    feat: binary search tree

--- data structure/tree/binarySearchTree.js
+++ data structure/tree/binarySearchTree.js
@@ -1,102 +1,107 @@
 
+class Node {
+    constructor(value) {
+        this.value = value
+        this.left = null
+        this.right = null
+        this.parent = null
+    }
+}
+
 class BinarySearchTree {
     constructor() {
         this.root = null
     }
 
     add(value) {
-        if (this.root) {
-            let currentNode = this.root
-            let newNode = {
-                value,
-                left: null,
-                right: null
-            }
-            while (currentNode) {
-                if (value < currentNode.value) {
-                    if (!currentNode.left) {
-                        return currentNode.left = newNode
-                    }
-                    currentNode = currentNode.left
-                } else {
-                    if (!currentNode.right) {
-                        return currentNode.right = newNode
-                    }
-                    currentNode = currentNode.right
-                }
-            }
+        let newNode = new Node(value)
+        if (!this.root) {
+            this.root = newNode
+            return
+        }
 
-        } else {
-            this.root = {
-                value,
-                left: null,
-                right: null
+        let pNode = null
+        let cNode = this.root
+        while(cNode) {
+            pNode = cNode
+            if (value < cNode.value) {
+                cNode = cNode.left
+            } else {
+                cNode = cNode.right
             }
         }
+        if (value < pNode.value) {
+            pNode.left = newNode
+        } else {
+            pNode.right = newNode
+        }
+        newNode.parent = pNode
     }
 
     delete(value) {
-        let currentNode = this.find(value)
-        if (!currentNode) {
-            throw new Error("Not found the value!!!")
+        let deleteNode = this.find(value)
+        if (!deleteNode) {
+            throw new Error("Not found the node!!!")
         }
 
-        let node = currentNode.node
-        let parent = currentNode.parentNode
-
+        let parent = deleteNode.parent
         // no left and right
-        if (!node.left && !node.right) {
+        if (!deleteNode.left && !deleteNode.right) {
             if (parent) {
-                parent.left === node ? parent.left = null : parent.right = null
+                parent.left === deleteNode ? parent.left = null : parent.right = null
             } else {
                 this.root = null
             }
         
         // no left but has right
-        } else if (!node.left) {
+        } else if (!deleteNode.left) {
             if (parent) {
-                parent.left === node ? parent.left = node.right : parent.right = node.right
+                parent.left === deleteNode ? parent.left = deleteNode.right : parent.right = deleteNode.right
             } else {
-                this.root = node.right
+                this.root = deleteNode.right
             }
+            deleteNode.right.parent = parent
 
         // no right but has left
-        } else if (!node.right) {
+        } else if (!deleteNode.right) {
             if (parent) {
-                parent.left === node ? parent.left = node.left : parent.right = node.left
+                parent.left === deleteNode ? parent.left = deleteNode.left : parent.right = deleteNode.left
             } else {
-                this.root = node.left
+                this.root = deleteNode.left
             }
+            deleteNode.left.parent = parent
 
         // has left and right
         } else {
-            let max = this.findMaxNode(node.left)
-            let mParent = max.parentNode
-            let mNode = max.node
-            mParent.left === mNode ? mParent.left = null : mParent.right = null
-            mNode.left = node.left
-            mNode.right = node.right
+            let nextNode = this.findMinNode(deleteNode.right)
+            if (nextNode !== deleteNode.right) {
+                nextNode.parent.left = nextNode.right
+                nextNode.right && (nextNode.right.parent = nextNode.parent)
+                nextNode.left = deleteNode.left
+                deleteNode.left && (deleteNode.left.parent = nextNode)
+                nextNode.right = deleteNode.right
+                deleteNode.right && (deleteNode.right.parent = nextNode)
+            } else {
+                nextNode.left = deleteNode.left
+                deleteNode.left && (deleteNode.left.parent = nextNode)
+            }
 
             if (parent) {
-                parent.left === node ? parent.left = mNode : parent.right = mNode
+                parent.left === deleteNode ? parent.left = nextNode : parent.right = nextNode
             } else {
-                this.root = mNode
+                this.root = nextNode
             }
+            nextNode.parent = parent
         }
 
     }
 
     find(value) {
         let currentNode = this.root
-        let parentNode = null
         while (currentNode) {
             if (value === currentNode.value) {
-                return {
-                    parentNode,
-                    node: currentNode
-                }
+                return currentNode
             } else {
-                parentNode = currentNode
                 if (value < currentNode.value) {
                     currentNode = currentNode.left
                 } else {
@@ -108,59 +113,56 @@ class BinarySearchTree {
     }
 
     findMinNode(node) {
-        let preNode = node
-        let parentNode = null
         while (node.left) {
-            parentNode = node
             node = node.left
-            preNode = node
         }
-        return {
-            parentNode,
-            node: preNode
-        }
+        return node
     }
 
     findMaxNode(node) {
-        let nextNode = node
-        let parentNode = null
         while (node.right) {
-            parentNode = node
             node = node.right
-            nextNode = node
         }
-        return {
-            parentNode,
-            node: nextNode
-        }
-
+        return node
     }
 
-    preOrder(node) {
-        console.log(node.value)
-        node.left && this.print(node.left)
-        node.right && this.print(node.right)
+    preOrder() {
+        let result = []
+        _inOrder(this.root)
+        return result.join(',')
+        function _inOrder(node) {
+            result.push(node.value)
+            node.left && _inOrder(node.left)
+            node.right && _inOrder(node.right)
+        }
+    }
+    inOrder() {
+        let result = []
+        _inOrder(this.root)
+        return result.join(',')
+        function _inOrder(node) {
+            node.left && _inOrder(node.left)
+            result.push(node.value)
+            node.right && _inOrder(node.right)
+        }
     }
 }
 
-function testAdd() {
+function testDelete(value) {
     let bst = new BinarySearchTree()
-    bst.add(3)
-    bst.add(2)
-    bst.add(4)
-    bst.add(1)
-    bst.add(2.5)
-    // bst.print(bst.root)
-    // console.log(bst.find(2))
-    // console.log(bst.find(8))
-    // console.log(bst.findMinNode(bst.root.left))
-    // console.log(bst.findMaxNode(bst.root.left))
-    bst.delete(3)
-    console.log(bst)
-    bst.delete(2)
-    console.log(bst)
-    bst.delete(4)
-    console.log(bst)
+    let values = [15, 6, 3, 2, 4, 7, 13, 9, 18, 17, 20]
+    values.forEach(v => {
+        bst.add(v)
+    })
+    console.log(bst.preOrder())
+    console.log(bst.inOrder())
+    value.forEach(v => {
+        bst.delete(v)
+        if (bst.root) {
+            console.log(`delete ${v}: `)
+            console.log(bst.preOrder(), bst.inOrder())
+        }
+    })
 }
 
-testAdd()
+testDelete([15, 6, 3, 2, 4, 7, 13, 9, 18, 17, 20])
